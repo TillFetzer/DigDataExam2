084827bd5d8e097d761c16211fc935f5926aa19d
Your Name <you@example.com>
2019-11-11 14:17:44 +0000

    method

--- magic.py
+++ magic.py
@@ -26,4 +26,34 @@ args = {
 
 dag = DAG('IMDb', default_args=args, description='IMDb Import',
           schedule_interval='56 18 * * *',
-          start_date=datetime(2019, 10, 16), catchup=False, max_active_runs=1)
+          start_date=datetime(2019, 10, 16), catchup=False, max_active_runs=1
+          )
+
+create_local_import_dir = CreateDirectoryOperator(
+    task_id='create_import_dir',
+    path='/home/airflow',
+    directory='imdb',
+    dag=dag,
+)
+
+clear_local_import_dir = ClearDirectoryOperator(
+    task_id='clear_import_dir',
+    directory='/home/airflow/imdb',
+    pattern='*',
+    dag=dag,
+
+)
+download_cards_method  = HttpDownloadOperator(
+        task_id='download_cards',
+    download_uri='https://api.magicthegathering.io/v1/cards/4711',
+    save_to='/home/airflow/imdb/cards_{{ ds }}.json',
+    dag=dag,
+)
+dummy_op = DummyOperator(
+        task_id='dummy', 
+        dag=dag)
+
+create_local_import_dir >> clear_local_import_dir
+create_local_import_dir >> clear_local_import_dir >> download_cards_method  >> dummy_op
+
+
