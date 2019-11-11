571180743f9e3cb57a99f24ff96f682b89c8c9a2
Your Name <you@example.com>
2019-11-11 14:53:12 +0000

    table

--- magic.py
+++ magic.py
@@ -23,37 +23,70 @@ from airflow.operators.hive_operator import HiveOperator
 args = {
     'owner': 'airflow'
 }
+hiveSQL_create_cards = '''
+CREATE EXTERNAL TABLE IF NOT EXISTS(
+   name STRING,
+   multiverseid DECIMAL(4,0),
+   artist STRING )
+'''
 
-dag = DAG('IMDb', default_args=args, description='IMDb Import',
+'''
+CREATE EXTERNAL TABLE IF NOT EXISTS title_basics(
+	tconst STRING,
+	title_type STRING,
+	primary_title STRING,
+	original_title STRING,
+	is_adult DECIMAL(1,0),
+	start_year DECIMAL(4,0),
+	end_year STRING,
+	runtime_minutes INT,
+	genres STRING
+) COMMENT 'IMDb Movies' PARTITIONED BY (partition_year int, partition_month int, partition_day int) ROW FORMAT DELIMITED FIELDS TERMINATED BY '\\t' STORED AS TEXTFILE LOCATION '/user/hadoop/imdb/title_basics'
+TBLPROPERTIES ('skip.header.line.count'='1');
+'''
+dag = DAG('Magic', default_args=args, description='IMDb Import',
           schedule_interval='56 18 * * *',
-          start_date=datetime(2019, 10, 16), catchup=False, max_active_runs=1
+          start_date=datetime(2019, 11, 11), catchup=False, max_active_runs=1
           )
 
 create_local_import_dir = CreateDirectoryOperator(
     task_id='create_import_dir',
     path='/home/airflow',
-    directory='imdb',
+    directory='magic',
     dag=dag,
 )
 
 clear_local_import_dir = ClearDirectoryOperator(
     task_id='clear_import_dir',
-    directory='/home/airflow/imdb',
+    directory='/home/airflow/magic',
     pattern='*',
     dag=dag,
 
 )
 download_cards_method  = HttpDownloadOperator(
         task_id='download_cards',
-    download_uri='https://api.magicthegathering.io/v1/cards/4711',
-    save_to='/home/airflow/imdb/cards_{{ ds }}.json',
+    download_uri='https://api.magicthegathering.io/v1/cards/',
+    save_to='/home/airflow/magic/cards_{{ ds }}.json',
     dag=dag,
 )
+hdfs_put_cards = HdfsPutFileOperator(
+    task_id='upload_cards_to_hdfs',
+    local_file='/home/airflow/magic/cards_{{ ds }}.json',
+    remote_file='/user/hadoop/mtg/raw/cards_{{ ds }}.json',
+    hdfs_conn_id='hdfs',
+    dag=dag,
+)
+create_HiveTable_cards = HiveOperator(
+    task_id='create_cards_table',
+    hql=hiveSQL_create_cards,
+    hive_cli_conn_id='beeline',
+    dag=dag)
+
 dummy_op = DummyOperator(
         task_id='dummy', 
         dag=dag)
 
 create_local_import_dir >> clear_local_import_dir
-create_local_import_dir >> clear_local_import_dir >> download_cards_method  >> dummy_op
+create_local_import_dir >> clear_local_import_dir >> download_cards_method >> hdfs_put_cards
 
 
